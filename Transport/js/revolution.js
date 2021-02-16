7a305d810723862394c8be4a026f94d6ab75f125
faisalniaz569 <73920072+faisalniaz569@users.noreply.github.com>
2021-02-16 12:08:36 +0600

    revolution

---
+++ Transport/js/revolution.js
@@ -0,0 +1,9 @@
+var $, document, jQuery;
+
+$(document).ready(function () {
+    "use strict";
+    // jQuery methods go here...
+    jQuery(".banner").revolution({
+        delay: 9000
+    });
+});
